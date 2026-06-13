f89b11a2907b7e79ebf2ed77f9a89b9c91ef584b
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-06-13 10:17:02 +0530

    add MediGoApplicationTests file

---
+++ src/test/java/com/medigo/MediGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
