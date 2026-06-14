3b4f3c14f2f1001a5fb11f2552bc490f34da8222
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-06-14 13:06:21 +0530

    add MediGoApplication file

---
+++ src/main/java/com/medigo/MediGoApplication.java
@@ -0,0 +1,19 @@
+package com.medigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediGoApplication.class, args);
+		/*
+		 * System.out.println("Hello Spring Boot");
+		 * 
+		 * System.out.println("Java Spring Boot"); System.out.println("Java");
+		 * System.out.println("Java");
+		 */
+	}
+
+}
