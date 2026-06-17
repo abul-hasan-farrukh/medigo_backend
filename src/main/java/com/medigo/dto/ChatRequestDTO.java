f524077092343aa4080c3de296546471d8358cf9
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-06-17 12:31:44 +0530

    add ChatRequestDTO file

---
+++ src/main/java/com/medigo/dto/ChatRequestDTO.java
@@ -0,0 +1,14 @@
+package com.medigo.dto;
+
+public class ChatRequestDTO {
+
+    private String question;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
