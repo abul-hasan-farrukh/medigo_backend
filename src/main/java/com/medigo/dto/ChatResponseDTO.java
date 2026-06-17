d43c15f210ac8e1b9ef3d581bfb41af15dcf5791
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-06-17 12:31:21 +0530

    add ChatResponseDTO file

---
+++ src/main/java/com/medigo/dto/ChatResponseDTO.java
@@ -0,0 +1,18 @@
+package com.medigo.dto;
+
+public class ChatResponseDTO {
+
+    private String response;
+
+    public ChatResponseDTO(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
